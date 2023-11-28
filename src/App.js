8ad85459b52300468bb4322129e824f2f15ebbe9
Andreia Basile Arruda <andreia.basile@gmail.com>
2023-11-28 10:05:13 -0500

    installed SCSS

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This will be my portfolio.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/deiabasile"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Github Account
         </a>
       </header>
     </div>
