2dbfabfe0f3933f45e8bb122ca146826232da7fe
Andreia Basile Arruda <andreia.basile@gmail.com>
2024-02-05 09:04:30 -0500

    PORTFOLIO-0016: new component for languague switch

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,22 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import classes from './App.module.scss';
 import Button from './components/button';
+import Header from './components/header';
 
 function App() {
-	const { t, i18n } = useTranslation();
-
-	const changeLanguageHandler = (e) => {
-		const languageValue = e.target.value;
-		i18n.changeLanguage(languageValue);
-	};
+	const { t } = useTranslation();
 
 	return (
 		<div className={classes.layoutWrapper}>
-			<header />
+			<header>
+				<Header />
+			</header>
 
 			<main>
 				<Button
 					name="name test 2"
 					id="123"
 					onButtonClick={() => alert('Hello world 1!')}
-					i18nKey="description.part2"
 					label={t('button')}
 					type="primary"
 				/>
@@ -31,20 +28,7 @@ function App() {
 					label={t('button')}
 					type="secondary"
 				/>
-				<div className="App">
-					{/* Select box to change language */}
-					<select
-						className="custom-select"
-						style={{ width: 200 }}
-						onChange={changeLanguageHandler}
-					>
-						<option value="en">English</option>
-						<option value="fr">French</option>
-						<option value="pt">Portuguese</option>
-					</select>
-					{/* call name of the variable from  the translation.json file to t() method */}
-					<h1>{t('andreia')}</h1>
-				</div>
+				<h1>{t('welcomeMessage')}</h1>
 			</main>
 
 			<footer />
