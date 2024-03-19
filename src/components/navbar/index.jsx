f80608e4d4bf3e248690792b262a2a737c21148a
Andreia Basile Arruda <andreia.basile@gmail.com>
2024-03-19 09:35:19 -0400

    PORTFOLIO-0022: implement React Router

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 // import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import useScreenDetector from '../../hooks/useScreenDetector';
 import Button from '../button';
 import classes from './Navbar.module.scss';
@@ -8,14 +9,9 @@ import classes from './Navbar.module.scss';
 function Navbar({ menuItems }) {
 	// const { t } = useTranslation();
 
-	// const [isNavVisible, setIsNavVisible] = useState(true);
 	const [isTabletNavVisible, setIsTabletNavVisible] = useState(false);
-	// console.log('test', isNavVisible);
 
 	const { isTablet, isMobile } = useScreenDetector();
-	// console.log('desktop', isDesktop);
-	// console.log('tablet', isTablet);
-	// console.log('mobile', isMobile);
 
 	const handleIsNavVisible = () => {
 		if (isTablet || isMobile) {
@@ -26,22 +22,13 @@ function Navbar({ menuItems }) {
 	};
 
 	useEffect(() => {
-		// console.log('2', isTablet);
 		handleIsNavVisible();
-
-		console.log('use Effect ran');
 	}, [isTablet, isMobile]);
-
 	return (
 		<div className={classes.wrapper}>
 			<div className={classes.burgerButtonWrapper}>
 				<Button
-					// why this doesnt worK?
-					// name="name test 2"
-					// id="123"
 					onButtonClick={() => setIsTabletNavVisible(!isTabletNavVisible)}
-					// label={t('button')}
-					// type="primary"
 				/>
 			</div>
 			{(isTabletNavVisible || (!isTablet && !isMobile)) && (
@@ -52,7 +39,15 @@ function Navbar({ menuItems }) {
 					<ul>
 						{menuItems.map((item) => (
 							<li key={item}>
-								<a href={`/${item.replace(/\s/g, '').toLowerCase()}`}>{item}</a>
+								<Link
+									to={`/${item
+										.replace(/\s/g, '')
+										.replace(/[áàÀ]/g, 'a')
+										.replace(/í/g, 'i')
+										.toLowerCase()}`}
+								>
+									{item}
+								</Link>
 							</li>
 						))}
 					</ul>
